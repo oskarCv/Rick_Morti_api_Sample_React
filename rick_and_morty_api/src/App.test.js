0ffe22b83abdd5f9bcf84ebec19d50acd789b94e
oskacr Cv <oskar.cv91@gmail.com>
2022-09-26 18:33:02 -0500

    Fix unit test

--- rick_and_morty_api/src/App.test.js
+++ rick_and_morty_api/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/simple app/i);
+  const linkElement = screen.getByText(/Rick and Morty/i);
   expect(linkElement).toBeInTheDocument();
 });
