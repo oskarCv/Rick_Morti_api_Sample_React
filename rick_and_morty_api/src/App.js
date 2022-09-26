67ea316eeecd53b6f9059e4a2eb06ce6644f1d5d
oskacr Cv <oskar.cv91@gmail.com>
2022-09-26 17:33:32 -0500

    Displaying characters

--- rick_and_morty_api/src/App.js
+++ rick_and_morty_api/src/App.js
@@ -1,16 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {getCharacters} from "./service/service";
 function App() {
+  const [characters, setCharacters] = useState([]);
   useEffect(()=>{
     const fetchChracterData = async () => {
       const response = await getCharacters();
-      console.log('ocv response -> ', response);
+      setCharacters(response.data.results);
+      console.log('ocv response -> ', response.data.results);
     }
     fetchChracterData();
   },[]);
   return (
     <div>
-      simple App
+      <h1>Rick and Morty</h1>
+      {
+        characters.map((character) => {
+          return (
+            <div key={character.id}>
+              <h2>{character.name}</h2>
+              <img src={character.image} alt={character.name} />
+            </div>
+          );
+        })
+      }
     </div>
   );
 }
